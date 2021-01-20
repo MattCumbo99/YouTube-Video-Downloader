5317b333d9e5f24066d6527d93a9a81eb2870d77
Matthew Cumbo <mattcumbo99@gmail.com>
2021-01-20 10:34:12 -0600

    Initial Commit

---
+++ src/main/java/com/mattrition/youtubevideodownloader/MainClass.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mattrition.youtubevideodownloader;
+
+/**
+ *
+ * @author Mattrition
+ */
+public class MainClass {
+    
+    public static void Main(String[] args) {
+        new MainFrame().setVisible(true);
+    }
+    
+}
